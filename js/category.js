dbbd633fb430e66b0fe99444b328968156268f19
Nguyễn Thị Bích Tuệ <95497729+Tue811@users.noreply.github.com>
2023-04-22 10:28:59 +0700

    fix

--- js/category.js
+++ js/category.js
@@ -23,18 +23,18 @@ rangeInput.forEach((input) => {
     });
   });
 
-  rangePrice.forEach((input) => {
-    input.addEventListener("input", (e) => {
-      let minPrice = rangePrice[0].value;
-      let maxPrice = rangePrice[1].value;
-      if (maxPrice - minPrice >= rangeMin && maxPrice <= rangeInput[1].max) {
-        if (e.target.className === "min") {
-          rangeInput[0].value = minPrice;
-          range.style.left = (minPrice / rangeInput[0].max) * 100 + "%";
-        } else {
-          rangeInput[1].value = maxPrice;
-          range.style.right = 100 - (maxPrice / rangeInput[1].max) * 100 + "%";
-        }
-      }
-    });
-  });
+  // rangePrice.forEach((input) => {
+  //   input.addEventListener("input", (e) => {
+  //     let minPrice = rangePrice[0].value;
+  //     let maxPrice = rangePrice[1].value;
+  //     if (maxPrice - minPrice >= rangeMin && maxPrice <= rangeInput[1].max) {
+  //       if (e.target.className === "min") {
+  //         rangeInput[0].value = minPrice;
+  //         range.style.left = (minPrice / rangeInput[0].max) * 100 + "%";
+  //       } else {
+  //         rangeInput[1].value = maxPrice;
+  //         range.style.right = 100 - (maxPrice / rangeInput[1].max) * 100 + "%";
+  //       }
+  //     }
+  //   });
+  // });
