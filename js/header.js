ef87e8a082fbdb9fe923a5bbdedce35072ae95a6
Nguyễn Thị Bích Tuệ <95497729+Tue811@users.noreply.github.com>
2023-04-24 08:50:53 +0700

    update

--- js/header.js
+++ js/header.js
@@ -449,6 +449,7 @@ var sticky = header.offsetTop;
 function myFunction() {
   if (window.pageYOffset > sticky) {
     header.classList.add("sticky");
+    
   } else {
     header.classList.remove("sticky");
   }
